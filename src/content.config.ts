a601a1b82df5f1c7e7549a88d1dc47e3bee4127c
yruyrui <saito.ryuto.ss8@naist.ac.jp>
2026-05-31 21:26:49 +0900

    first post

---
+++ src/content.config.ts
@@ -0,0 +1,16 @@
+import { z, defineCollection } from 'astro:content';
+import { glob } from 'astro/loaders';
+
+const blogCollection = defineCollection({
+  loader: glob({ pattern: "**/*.md", base: "./src/content/blog" }),
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    pubDate: z.date(),
+    tags: z.array(z.string()).optional(),
+  }),
+});
+
+export const collections = {
+  'blog': blogCollection,
+};
---
+++ src/content/blog/first-post.md
@@ -0,0 +1,14 @@
+---
+title: "Hello, World!"
+description: "ブログはじめました"
+pubDate: 2026-03-31
+tags: ["blog", "astro", "cloudflare"]
+---
+
+言語モデルに雇用だけでなく前頭前野まで破壊されるところだったので, リハビリも兼ねてブログ始めてみました.
+ちなみに, このサイトは [Astro](https://astro.build/) で構築し, Cloudflare Pages で公開しています.
+
+また, ここまでの日本語は [SKK](https://dic.nicovideo.jp/a/skk) で打ちこまれています.
+慣れないにゃあ...
+
+### Hello, World!
